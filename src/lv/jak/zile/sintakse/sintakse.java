4061b9b3d27981349d61e0cad38b88c1251e57d1
lietotajs <lietotajs@121-18>
2019-12-20 10:57:46 +0200

    Aprakstīta klase SINTAKSE

---
+++ src/lv/jak/zile/sintakse/sintakse.java
@@ -0,0 +1,27 @@
+// programmas koda daļa, kas palīdz programmas izstrādātājiem labāk izprast koda darbību principus.
+// programmas darbības laikā komentāri netiek ņemti vērā.
+
+// - vienas rindas komentārs
+
+/*
+ * vairāku
+ * rindu
+ * komentārs
+ */
+
+package lv.jak.zile.sintakse; // pakotne - klases atrašanās vieta projektā
+
+// import komanda - komandarinda, kas nodrošina citas klases vai pakotnes piesaisti šai klasei.
+// tādā veidā tiek nodrošinātas dažād funkciju izmantošanas iespējas šajā klasē, piemēram, 
+// datu ievade no tastatūras.
+
+public class sintakse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
