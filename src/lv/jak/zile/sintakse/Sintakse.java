22957b1c97d4de19f1a1d139c8313c0d0440f9e4
lietotajs <lietotajs@121-18>
2019-12-20 11:00:13 +0200

    Sintakses klase

---
+++ src/lv/jak/zile/sintakse/Sintakse.java
@@ -0,0 +1,71 @@
+// programmas koda daļa, kas palīdz programmas izstrādātājiem labāk izprast koda darbību principus.
+// programmas darbības laikā komentāri netiek ņemti vērā.
+
+// - vienas rindas komentārs
+
+/*
+ * vairāku
+ * rindu
+ * komentārs
+ */
+
+package lv.jak.zile.sintakse; // pakotne - klases atrašanās vieta projektā
+
+// import komanda - komandarinda, kas nodrošina citas klases vai pakotnes piesaisti šai klasei.
+// tādā veidā tiek nodrošinātas dažād funkciju izmantošanas iespējas šajā klasē, piemēram, 
+// datu ievade no tastatūras.
+/* 
+ * Iespējamās pieraksta formas
+ * import <pakotne>.<Klase>;
+ * import <pakotne>.*;
+ */
+ import java.util.Scanner;
+
+ // JAVA Klases daļa - galvenā programmēšanas daļa
+ // Sintakse - Klases nosaukums
+ /*
+  * Sintakses nosacījumi uz klases nosaukumu
+  *      1. Klases nosaukums sākas ar lielo burtu
+  *      2. Ja klases nosaukums satur vairākus vārdus, katrs vārds sākas ar lielo burtu
+  *      3. Nosaukums var saturēt TIKAI BURTUS UN CIPARUS!!!
+  *      4. Ja klases nosaukums satur vairākus vārdus, tos ar atstarpēm NEATDALA!!!
+  */
+public class Sintakse {
+    // Klases ķermenis - šajā daļā aprakstītais koda fragments ir pieejams visai klasei,
+	// izņēmuma nosacījums - aprakstītās lietas ir redzamas tikai tai daļai koda, kas atrodas zemāk!
+    // !!! NEDRĪKST rakstīt funkcionālus procesus, izņemot vērtības piešķiršanu vai objekta izveidi!
+	
+	// Mainīgie - programmēšanas valodas pamatelements
+	/*
+	  * Sintakses nosacījumi uz klases nosaukumu
+	  *      1. Mainīga nosaukums sākas ar mazo burtu
+	  *      2. Ja mainīga nosakums satur vairākus vārdus, pirmais vārds ar mazo, katrs nākamais vārds sākas ar lielo burtu
+	  *      3. Ja klases nosaukums satur vairākus vārdus, tos ar atstarpēm NEATDALA!!!
+	  *      4. Nosakumus var saturēt  TIKAI BURTUS UN CIPARUS!!!
+	  *      
+	  * Izņēmuma gadījumi:
+	  *      1.
+	  *      2.
+    */
+   // Mainīga izveidošana
+   /*
+    * Pieraksta forma:
+    *      --> ja iepriekš mainīgais NAV izveidots: <datuTips> <mainigaNosaukums>;
+    */
+	int veselsSkaitlis;
+	
+	// metode MAIN - galvenā metodi.
+	/*
+	 *  MAIN metode:
+	 *       1. UZ projektu jābūt VISMAZ 1 MAIN metodei
+	 *       2. Metodes pieraksts ir TIEŠI tāds, kā redzams zemāk pierakstā
+	 */
+	public static void main(String[] args) {
+		//metodes ķermenis - aprakstam algoritmu, kuru vēlamies izpildīt	
+	}
+
+ // Klases ķermenis ir beidzies
+}
+// !!! NEKO ŠEIT NEDRĪKST RAKSTĪT.
+
+
